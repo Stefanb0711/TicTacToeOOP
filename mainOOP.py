ac0c785974af7adbfe528c8a90bf8656993ec19a
Stefanb0711 <stefanboehme12@gmail.com>
2024-05-18 14:43:43 +0200

    Fertig

--- mainOOP.py
+++ mainOOP.py
@@ -1,11 +1,12 @@
-import numpy as np
 import sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from TicTacToe import TicTacToe, Session, Spielstand
+import random
 
 
-engine = create_engine('sqlite:///spielstand.db', echo=False)
+"""engine = create_engine('sqlite:///spielstand.db', echo=False)
 
 # Erstelle eine "Base" Klasse als Basis für ORM-Modelle
 Base = sqlalchemy.orm.declarative_base()
@@ -17,12 +18,14 @@ class Spielstand(Base):
     spieler_0 = Column(Integer)
 
 
+
 Base.metadata.create_all(engine)
 
 # Erstelle eine Session-Fabrik
 Session = sessionmaker(bind=engine)
-session = Session()
+session = Session()"""
 
+session = Session()
 
 aktueller_spielstand = session.query(Spielstand).filter(Spielstand.id == 1).first()
 if not aktueller_spielstand:
@@ -30,149 +33,42 @@ if not aktueller_spielstand:
     session.add(spielstand_erstellen)
     session.commit()
 
-"""if aktueller_spielstand.spieler_0 == None or aktueller_spielstand.spieler_x == None:
-    aktueller_spielstand.spieler_x = 0
-    aktueller_spielstand.spieler_0 = 0
-    session.commit()"""
 
-class TicTacToe:
-    def __init__(self):
-        self.spieler_x_eingabe = ""
-        self.spieler_0_eingabe = ""
-        self.spielstand_x = 0
-        self.spielstand_0 = 0
-        self.spiel_zu_ende = False
-        self.gewonnen_x = False
-        self.gewonnen_0 = False
-        self.pos = np.array([["1","2","3"], ["4","5","6"], ["7","8","9"]])
-
-
-    def checken_sieg(self, spieler):
-        for zeile in self.pos:
-            reihe_voll = np.all(zeile == spieler)
-
-            if reihe_voll == True:
-                print(f"{spieler} hat gewonnen")
-                if spieler == "X":
-                    self.gewonnen_x = True
-                    self.spiel_zu_ende = True
-                    break
-                elif spieler == "0":
-                    self.gewonnen_0 = True
-                    self.spiel_zu_ende = True
-
-
-
-        # Vertikal checken
-        for werte in range(3):
-            vertikal_werte = self.pos[:, werte]
-            reihe_voll = np.all(vertikal_werte == spieler)
-
-            if reihe_voll:
-                print(f"{spieler} hat gewonnen")
-                if spieler == "X":
-                    self.gewonnen_x = True
-                    self.spiel_zu_ende = True
-                    break
-                elif spieler == "0":
-                    self.gewonnen_0 = True
-                    self.spiel_zu_ende = True
-
-
-
-        # Diagonal checken
-        for wert in range(3):
-            if not self.pos[wert][wert] == spieler:
-                break
-            else:
-                if wert == 2:
-                    print(f"{spieler} hat gewonnen")
-                    if spieler == "X":
-                        self.gewonnen_x = True
-                        self.spiel_zu_ende = True
-                        break
-                    elif spieler == "0":
-                        self.gewonnen_0 = True
-                        self.spiel_zu_ende = True
-
-
-
-    def display_board(self):
-        tic_tac_toe_bord = f"{self.pos[0][0]} | {self.pos[0][1]} | {self.pos[0][2]} \n" \
-                           f"---------\n" \
-                           f"{self.pos[1][0]} | {self.pos[1][1]} | {self.pos[1][2]} \n" \
-                           f"---------\n" \
-                           f"{self.pos[2][0]} | {self.pos[2][1]} | {self.pos[2][2]} \n "
-        print(tic_tac_toe_bord)
-
-
-    def spielstand(self):
-
-        if self.gewonnen_x:
-            aktueller_spielstand.spieler_x += 1
-            session.commit()
-            """elf.spielstand_x += 1
-            with open("gespeicherter_spielstand.txt", "a") as datei:
-                datei.write("")"""
-
-            print(f"Spielstand: \n X = {aktueller_spielstand.spieler_x}\n Y = {aktueller_spielstand.spieler_0}")
-        elif self.gewonnen_0:
-            aktueller_spielstand.spieler_0 += 1
-            session.commit()
-            print(f"Spielstand: \n X = {aktueller_spielstand.spieler_x}\n Y = {aktueller_spielstand.spieler_0}")
-
-
-
-    def benutzereingabe_und_feld_setzen(self, spieler):
-        if spieler == "X":
-            self.spieler_x_eingabe = input("Spieler X: Geben Sie eine Zahl von 1 - 9 ein:")
-            if not tic_tac_toe.spieler_x_eingabe.isdigit():
-                print("Der eingegebene Wert ist nicht gültig. Versuchen Sie es nochmal")
-                #continue
-            elif int(tic_tac_toe.spieler_x_eingabe) > 9 or int(tic_tac_toe.spieler_x_eingabe) < 1:
-                print("Der eingegebene Wert ist nicht gültig")
-                #continue
-            spieler_x_position = np.where(self.pos == self.spieler_x_eingabe)
-            self.pos[spieler_x_position] = "X"
-
-        elif spieler == "0":
-            self.spieler_0_eingabe = input("Spieler 0: Geben Sie eine Zahl von 1 - 9 ein:")
-            if not tic_tac_toe.spieler_0_eingabe.isdigit():
-                print("Der eingegebene Wert ist nicht gültig. Versuchen Sie es nochmal")
-                #continue
-            elif int(tic_tac_toe.spieler_0_eingabe) > 9 or int(tic_tac_toe.spieler_0_eingabe) < 1:
-                print("Der eingegebene Wert ist nicht gültig")
-                #continue
-
-            spieler_0_position = np.where(self.pos == self.spieler_0_eingabe)
-            self.pos[spieler_0_position] = "0"
-
-
-
-
+aktueller_spielstand.spieler_x = 0
+aktueller_spielstand.spieler_0 = 0
+session.commit()
 
 
 
 tic_tac_toe = TicTacToe()
-aktueller_spielstand = session.query(Spielstand).filter(Spielstand.id == 1).first()
+#aktueller_spielstand = session.query(Spielstand).filter(Spielstand.id == 1).first()
 
-print(f"Aktueller Spielstand: \n Spieler X: {aktueller_spielstand.spieler_x} Spieler 0: {aktueller_spielstand.spieler_0}")
-while not tic_tac_toe.spiel_zu_ende:
+#print(f"Aktueller Spielstand: \n Spieler X: {aktueller_spielstand.spieler_x} Spieler 0: {aktueller_spielstand.spieler_0}")
 
+aktueller_spieler = random.choice(["X", "0"])
 
-    tic_tac_toe.benutzereingabe_und_feld_setzen("X")
-    tic_tac_toe.display_board()
-    tic_tac_toe.checken_sieg("X")
-    if tic_tac_toe.gewonnen_x:
-        tic_tac_toe.spielstand()
-        continue
+while True:
+    if aktueller_spieler == "X":
+        tic_tac_toe.benutzereingabe_und_feld_setzen("X")
+        tic_tac_toe.display_board()
+        tic_tac_toe.checken_sieg("X")
+        aktueller_spieler = "0"
 
-    tic_tac_toe.benutzereingabe_und_feld_setzen("0")
-    tic_tac_toe.display_board()
-    tic_tac_toe.checken_sieg("0")
-    if tic_tac_toe.gewonnen_0:
-        tic_tac_toe.spielstand()
-        continue
+        if tic_tac_toe.gewonnen_x:
+            #aktueller_spielstand.spieler_x += 1
+            #session.commit()
+            tic_tac_toe.spielstand(aktueller_spielstand)
+
+            continue
+    else:
+        tic_tac_toe.benutzereingabe_und_feld_setzen("0")
+        tic_tac_toe.display_board()
+        tic_tac_toe.checken_sieg("0")
+        aktueller_spieler = "X"
+
+        if tic_tac_toe.gewonnen_0:
+            tic_tac_toe.spielstand(aktueller_spielstand)
+            continue
 
 """
 tic_tac_toe.spieler_x_eingabe = input("Spieler 0: Geben Sie eine Zahl von 1 - 9 ein: ")
